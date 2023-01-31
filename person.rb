26fdd085deacfad8ee619fc35802f5c296419746
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-01-31 16:59:23 +0630

    make  Person class inherits from Nameable

--- person.rb
+++ person.rb
@@ -1,16 +1,23 @@
-class Person
-  attr_reader :id
-  attr_accessor :name, :age
+require "./nameable"
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+class Person < Nameable
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
+  attr_reader :id
+  attr_accessor :name, :age
+
   def can_use_services?
     return true if @age >= 18 || @parent_permission == true
+  end
+
+  def correct_name
+    @name
   end
 
   private
