da8e7585c27145249b59f167952d97e3af0e5394
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-02-13 22:43:45 +0630

    Fix the errors for run method

--- run_app.rb
+++ run_app.rb
@@ -10,19 +10,19 @@ class RunApp
    def choose(choice)
     case choice
     when '1'
-      list_all_books
+      @app.list_all_books
     when '2'
-      list_all_people
+      @app.list_all_people
     when '3'
-      create_a_person
+      @app.create_a_person
     when '4'
-      create_a_book
+      @app.create_a_book
     when '5'
-      create_a_rental
+      @app.create_a_rental
     when '6'
-      list_all_rentals
+      @app.list_all_rentals
     end
-  end
+   end
 
  def run
     puts 'Welcome to the school library'
@@ -32,5 +32,5 @@ class RunApp
 
       choose(choice)
     end
-  end
+ end
  end
